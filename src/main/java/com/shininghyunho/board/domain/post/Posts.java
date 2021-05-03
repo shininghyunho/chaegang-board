ef2d53553f5b96e8fb82777bd3181ab8a6a6fe46
hyunho <gusgh3315@gmail.com>
2021-05-03 14:18:05 +0900

    add post domain & post repository & BaseTimeEntity

---
+++ src/main/java/com/shininghyunho/board/domain/post/Posts.java
@@ -0,0 +1,33 @@
+package com.shininghyunho.board.domain.post;
+
+import com.shininghyunho.board.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500,nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT",nullable = false)
+    private String content;
+
+    private String author; // author는 바꿀일이 없기때문에 컬럼 표시 x
+
+    @Builder
+    public Posts(String title,String content,String author){
+        this.title=title;
+        this.content=content;
+        this.author=author;
+    }
+}
---
+++ src/test/java/com/shininghyunho/board/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,71 @@
+package com.shininghyunho.board.domain.posts;
+
+import com.shininghyunho.board.domain.post.Posts;
+import com.shininghyunho.board.domain.post.PostsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        // given
+        String title="테스트 게시글 제목";
+        String content="테스트 게시글 내용";
+        String author="테스트 게시글 작성자";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build());
+
+        // when
+        List<Posts> postsList=postsRepository.findAll();
+
+        // then
+        Posts posts=postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+        assertThat(posts.getAuthor()).isEqualTo(author);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+        // given
+        LocalDateTime now = LocalDateTime.of(2021,5,3,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        // when
+        List<Posts> postsList=postsRepository.findAll();
+
+        // then
+        Posts posts=postsList.get(0);
+        System.out.println(">>>>>>>>>>>>>>> createDate = "+posts.getCreatedDate()+
+                ", modifiedDate = "+posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
+}
