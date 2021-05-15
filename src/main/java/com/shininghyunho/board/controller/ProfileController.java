9e98286fed5b3519123de23c6255d7b0490b3466
hyunho <gusgh3315@gmail.com>
2021-05-15 16:54:12 +0900

    test

--- src/main/java/com/shininghyunho/board/controller/ProfileController.java
+++ src/main/java/com/shininghyunho/board/controller/ProfileController.java
@@ -1,7 +1,6 @@
 package com.shininghyunho.board.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
-@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class ProfileController {
@@ -22,7 +20,6 @@ public class ProfileController {
         // real1,real2 무중단 배포용
         List<String> realProfiles = Arrays.asList("real","real1","real2");
         String defaultProfile = profiles.isEmpty()? "default":profiles.get(0);
-
         return profiles.stream()
                 .filter(realProfiles::contains)
                 .findAny()
